b8f424195b2ab0884f3e75653cad3a6b84650944
fazt <me@faztweb.com>
2017-09-24 21:06:19 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -3,74 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-let Tasks = [
-  {
-    id: 1,
-    title: 'My First Task',
-    description: 'this is the description of the task',
-    responsible: 'Fazt',
-    priority: 'High'
-  },
-  {
-    id: 2,
-    title: 'My First Task',
-    description: 'this is the description of the task',
-    responsible: 'Fazt',
-    priority: 'High'
-  },
-  {
-    id: 3,
-    title: 'My First Task',
-    description: 'this is the description of the task',
-    responsible: 'Fazt',
-    priority: 'High'
-  },
-  {
-    id: 4,
-    title: 'My First Task',
-    description: 'this is the description of the task',
-    responsible: 'Fazt',
-    priority: 'High'
-  },
-  {
-    id: 5,
-    title: 'My First Task',
-    description: 'this is the description of the task',
-    responsible: 'Fazt',
-    priority: 'High'
-  },
-  {
-    id: 6,
-    title: 'My First Task',
-    description: 'this is the description of the task',
-    responsible: 'Fazt',
-    priority: 'High'
-  },
-  {
-    id: 7,
-    title: 'My First Task',
-    description: 'this is the description of the task',
-    responsible: 'Fazt',
-    priority: 'High'
-  },
-  {
-    id: 8,
-    title: 'My First Task',
-    description: 'this is the description of the task',
-    responsible: 'Fazt',
-    priority: 'High'
-  },
-  {
-    id: 9,
-    title: 'My First Task',
-    description: 'this is the description of the task',
-    responsible: 'Fazt',
-    priority: 'High'
-  }
-];
+import { tasks } from './tasks.json';
 
 
 ReactDOM.render(
-  <App title="RTasks" tasks={Tasks}/>,
+  <App title="RTasks" tasks={tasks}/>,
   document.getElementById('root')
 );
